37875a8b7118f5ad7d0c50c27ea2226c4d417897
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2022-05-31 13:54:41 +0200

    feat: Basic implem for non-optional types

--- envload_derive/src/lib.rs
+++ envload_derive/src/lib.rs
@@ -1,20 +1,42 @@
-#![allow(unused)]
-
 use convert_case::Casing;
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Field, Fields, Ident};
 
 #[proc_macro_derive(Envload)]
-pub fn derive_envload(input: TokenStream) -> TokenStream {
+pub fn derive_envload(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let name = input.ident;
+    let fields: Vec<syn::Field> = match input.data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => fields.named.iter().map(|f| f.clone()).collect(),
+            Fields::Unnamed(_) => unimplemented!(),
+            Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) => unimplemented!(),
+        Data::Union(_) => unimplemented!(),
+    };
+
+    let mut mandatory_fields = vec![];
+    // let mut optional_fields = vec![];
+
+    for f in fields {
+        // TODO: Check whether the type is mandatory or optional!
+
+        mandatory_fields.push(f);
+    }
+
+    let mandatory_tt = gen_mandatory_tt(&mandatory_fields);
+    // let optional_tt = gen_optional_tt(&optional_fields);
+    let return_struct = gen_return_struct(&name, &mandatory_fields /*, optional_fields */);
 
     let expanded = quote! {
         impl envload::Envload for #name {
             fn load() -> #name {
-                unimplemented!()
+                #mandatory_tt
+
+                #return_struct
             }
 
             // fn try_load() -> Result<#name, envload::EnvloadError> {
@@ -23,8 +45,56 @@ pub fn derive_envload(input: TokenStream) -> TokenStream {
         }
     };
 
-    TokenStream::from(expanded)
+    proc_macro::TokenStream::from(expanded)
 }
+
+fn gen_mandatory_tt(fields: &[Field]) -> TokenStream {
+    let decls = fields.iter().map(|f| {
+        let name = f.ident.clone();
+        let key = f
+            .ident
+            .clone()
+            .expect("No field name")
+            .clone()
+            .to_string()
+            .to_case(convert_case::Case::ScreamingSnake);
+        let ty = f.ty.clone();
+
+        quote_spanned! {f.span()=>
+            let #name: #ty = std::env::var(#key)
+                .unwrap_or_else(|_| panic!("Environment variable not found"))
+                .parse()
+                .unwrap_or_else(|_| panic!("Couldn't parse environment variable"));
+        }
+    });
+
+    quote! {
+        #(#decls)*
+    }
+}
+
+fn gen_return_struct(name: &Ident, mandatory_fields: &[Field]) -> TokenStream {
+    let field_names = mandatory_fields.iter().map(|f| {
+        let name = f.ident.clone();
+        quote_spanned! {f.span()=>
+            #name,
+        }
+    });
+
+    quote! {
+        return #name {
+            #(#field_names)*
+        };
+    }
+}
+
+// fn is_optional(field: &Field) -> bool {
+//     match field.ty {
+//         _ => unimplemented!(),
+//     }
+
+//     return false;
+// }
 
 // fn f(data: &Data) -> TokenStream {
 //     match *data {
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,60 +1,32 @@
 use envload::Envload;
-// use envload_derive::Envload;
+use envload_derive::Envload;
 
-// #[derive(Envload)]
+#[derive(Envload)]
 struct Env {
     secret_key: String,
     int_data: i32,
-    optional_data: Option<usize>,
+    // optional_data: Option<usize>,
 }
-
-// The `Envload` impl derives to:
-// ```rust
-impl Envload for Env {
-    fn load() -> Env {
-        let secret_key: String = std::env::var("SECRET_KEY")
-            .unwrap_or_else(|_| panic!("Environment variable not found: SECRET_KEY"))
-            .parse()
-            .unwrap_or_else(|_| panic!("Couldn't parse environment variable: SECRET_KEY"));
-        let int_data: i32 = std::env::var("INT_DATA")
-            .unwrap_or_else(|_| panic!("Environment variable not found: INT_DATA"))
-            .parse()
-            .unwrap_or_else(|_| panic!("Couldn't parse environment variable: INT_DATA"));
-        let optional_data: Option<usize> = std::env::var("OPTIONAL_DATA").ok().map(|v| {
-            v.parse()
-                .unwrap_or_else(|_| panic!("Couldn't parse environment variable: OPTIONAL_DATA"))
-        });
-
-        // let optional_data = std::env::var("OPTIONAL_DATA").unwrap();
-
-        Env {
-            secret_key,
-            int_data,
-            optional_data,
-        }
-    }
-}
-// ```
 
 #[test]
-fn test_load_success() {
+fn load_success() {
     std::env::set_var("SECRET_KEY", "hunter2");
     std::env::set_var("INT_DATA", "128");
 
     let env = <Env as Envload>::load();
     assert_eq!(env.secret_key, "hunter2");
     assert_eq!(env.int_data, 128);
-    assert_eq!(env.optional_data, None);
+    // assert_eq!(env.optional_data, None);
 
-    std::env::set_var("OPTIONAL_DATA", "37");
+    // std::env::set_var("OPTIONAL_DATA", "37");
 
-    let env = <Env as Envload>::load();
-    assert_eq!(env.optional_data, Some(37));
+    // let env = <Env as Envload>::load();
+    // assert_eq!(env.optional_data, Some(37));
 }
 
 #[test]
 #[should_panic]
-fn test_load_failure() {
+fn load_failure() {
     std::env::remove_var("SECRET_KEY");
 
     <Env as Envload>::load();
